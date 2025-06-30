e2b52f2f8cb5d4edd88eb487954b2a2a3feb95b8
justvarma <146994690+justvarma@users.noreply.github.com>
2025-06-30 11:32:01 +0530

    update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-import josn
+import json
 import os
 
 st.set_page_config(page_title="Simple Finance App", page_icon="💸", layout="wide")
