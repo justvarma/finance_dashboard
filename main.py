be90d48ac945aab2438c13ce9bdd22dd564f619b
justvarma <146994690+justvarma@users.noreply.github.com>
2025-06-30 11:42:08 +0530

    update

--- main.py
+++ main.py
@@ -7,10 +7,14 @@ import os
 st.set_page_config(page_title="Simple Finance App", page_icon="💸", layout="wide")
 
 def load_transactions(file):
-    pass
+    try:
+        df = pd.read_csv(file)
+        
 
 def main():
     st.title("Finance Dashboard")
     uploadede_file=st.file_uploader("Upload your transaction CSV file", type=["csv"])
     if uploadede_file is not None:
         df = load_transactions(uploadede_file)
+
+main()
