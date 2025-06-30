5f5fcee1d02fb71f7479d285488112d1fbe9bdaa
justvarma <146994690+justvarma@users.noreply.github.com>
2025-06-30 12:07:04 +0530

    update

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ def load_transactions(file):
     try:
         df = pd.read_csv(file)
         df.columns = [col.strip() for col in df.columns]
+        df["Amount"] = df["Amount"].str.replace(",", "").astype(float)
+        df["Date"] = pd.to.datetime(df["Date"], format="%d %b %Y")
         st.write(df)
         return df
     except Exception as e:
@@ -23,6 +25,10 @@ def main():
     uploadede_file = st.file_uploader("Upload your transaction CSV file", type=["csv"])
     if uploadede_file is not None:
         df = load_transactions(uploadede_file)
+        if df is not None:
+            debits_df = df[df["Debit/Credit"] == "Debit"].copy()
+            credits_df = df[df["Debit/Credit"] == "Credit"].copy()
 
+            tab1, tab2 = str.tabs()
 
 main()
