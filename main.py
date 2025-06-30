86e4949cc668511939586a4cd4b361297a92e8f2
justvarma <146994690+justvarma@users.noreply.github.com>
2025-06-30 11:58:44 +0530

    update

--- main.py
+++ main.py
@@ -6,16 +6,23 @@ import os
 
 st.set_page_config(page_title="Simple Finance App", page_icon="💸", layout="wide")
 
+
 def load_transactions(file):
     try:
         df = pd.read_csv(file)
-    except Exception as e
+        df.columns = [col.strip() for col in df.columns]
+        st.write(df)
+        return df
+    except Exception as e:
+        st.error(f"Error processing file: {str(e)}")
+        return None
 
 
 def main():
     st.title("Finance Dashboard")
-    uploadede_file=st.file_uploader("Upload your transaction CSV file", type=["csv"])
+    uploadede_file = st.file_uploader("Upload your transaction CSV file", type=["csv"])
     if uploadede_file is not None:
         df = load_transactions(uploadede_file)
+
 
 main()
